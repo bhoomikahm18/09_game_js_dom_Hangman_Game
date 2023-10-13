fbde26d3f84879e8ef9d6a18434d3fed2afa105b
Bhoomika <bhoomikahm18@gmail.com>
2023-10-13 14:21:00 +0530

    Options values for buttons

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+//Initial References
+const letterContainer = document.getElementById("letter-container");
+const optionsContainer = document.getElementById("options-container");
+const userInputSection = document.getElementById("user-input-section");
+const newGameContainer = document.getElementById("new-game-container");
+const newGameButton = document.getElementById("new-game-button");
+const canvas = document.getElementById("canvas");
+const resultText = document.getElementById("result-text");
+
+//Options values for buttons
+let options = {
+    fruits: [
+      "Apple",
+      "Blueberry",
+      "Mandarin",
+      "Pineapple",
+      "Pomegranate",
+      "Watermelon",
+    ],
+    animals: ["Hedgehog", "Rhinoceros", "Squirrel", "Panther", "Walrus", "Zebra"],
+    countries: [
+      "India",
+      "Hungary",
+      "Kyrgyzstan",
+      "Switzerland",
+      "Zimbabwe",
+      "Dominica",
+    ],
+  };
