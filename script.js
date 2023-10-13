ebb55a4490faa51ea51fd23831477ee093b9f1bc
Bhoomika <bhoomikahm18@gmail.com>
2023-10-13 14:35:05 +0530

    Adding css property for options container

--- script.js
+++ script.js
@@ -43,3 +43,14 @@ const displayOptions = () => {
     }
     optionsContainer.appendChild(buttonCon);
 }
+
+//Initial function (Called when page loads/user presses new game)
+const initializer = () => {
+    winCount = 0;
+    count = 0;
+    displayOptions();
+};
+
+//New Game
+newGameButton.addEventListener("click", initializer);
+window.onload = initializer;
