87c01dd9d19081bdd91b18a16fa4e688fac22b35
Bhoomika <bhoomikahm18@gmail.com>
2023-10-13 15:45:08 +0530

    Drawing the man to game

--- script.js
+++ script.js
@@ -151,6 +151,33 @@ const initializer = () => {
     initialDrawing();
 };
 
+//draw the man
+const drawMan = (count) => {
+    let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
+    switch (count) {
+        case 1:
+            head();
+            break;
+        case 2:
+            body();
+            break;
+        case 3:
+            leftArm();
+            break;
+        case 4:
+            rightArm();
+            break;
+        case 5:
+            leftLeg();
+            break;
+        case 6:
+            rightLeg();
+            break;
+        default:
+            break;
+    }
+};
+
 //Canvas
 const canvasCreator = () => {
     let context = canvas.getContext("2d");
